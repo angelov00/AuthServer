54a00ef256eb655fee5dfd626244495e8d900c80
angelov00 <147129698+angelov00@users.noreply.github.com>
2025-03-15 18:25:35 +0200

    Add scheduled cleanup for expired sessions

--- src/main/java/com/angelov00/server/repository/InMemorySessionRepository.java
+++ src/main/java/com/angelov00/server/repository/InMemorySessionRepository.java
@@ -4,15 +4,29 @@ import com.angelov00.server.model.entity.Session;
 import com.angelov00.server.model.entity.User;
 
 import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
 public class InMemorySessionRepository {
 
     private final ConcurrentHashMap<String, Session> sessions;
+    private final ScheduledExecutorService executor;
 
     public InMemorySessionRepository() {
         sessions = new ConcurrentHashMap<>();
+        this.executor = Executors.newSingleThreadScheduledExecutor();
+
+        executor.scheduleWithFixedDelay(() -> {
+            Iterator<String> iterator = sessions.keySet().iterator();
+            while (iterator.hasNext()) {
+                String sessionId = iterator.next();
+                Session session = sessions.get(sessionId);
+                if (session != null && isExpired(session)) {
+                    iterator.remove();
+                }
+            }
+        }, 30, 5, TimeUnit.MINUTES);
     }
 
     public void invalidate(String sessionId) {
@@ -23,18 +37,23 @@ public class InMemorySessionRepository {
         return sessions.containsKey(sessionId);
     }
 
-    public void createSession(User user, int timeToLive) {
+    public String createSession(User user, int timeToLive) {
         String sessionId = UUID.randomUUID().toString();
         Session session = new Session(user, timeToLive);
         sessions.put(sessionId, session);
+        return sessionId;
     }
 
     public boolean isValid(String sessionId) {
         Session session = sessions.get(sessionId);
-        if (session != null && !session.getCreatedAt().plusSeconds(session.getTimeToLive()).isAfter(LocalDateTime.now())) {
+        if (session != null && isExpired(session)) {
             invalidate(sessionId);
             return false;
         }
         return session != null;
+    }
+
+    private boolean isExpired(Session session) {
+        return session.getCreatedAt().plusSeconds(session.getTimeToLive()).isBefore(LocalDateTime.now());
     }
 }
