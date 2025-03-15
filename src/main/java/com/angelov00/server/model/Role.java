75fb5af22ccda2accdc5975b4ce003c347e9aa10
angelov00 <147129698+angelov00@users.noreply.github.com>
2025-03-15 12:15:00 +0200

    Add User entity

---
+++ src/main/java/com/angelov00/server/model/Role.java
@@ -0,0 +1,5 @@
+package com.angelov00.server.model;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/angelov00/server/model/User.java
@@ -0,0 +1,33 @@
+package com.angelov00.server.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {}
+
+}
